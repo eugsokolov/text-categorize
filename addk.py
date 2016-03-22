18cf1d290f43d4ffb442cce2bfd09b1930fd1c28
eugene sokolov <eugsokolov@gmail.com>
2016-03-22 15:42:55 -0400

    fixed file structure inputs

--- addk.py
+++ addk.py
@@ -1,22 +1,33 @@
 from nltk import word_tokenize
+from nltk.stem.lancaster import LancasterStemmer
+from nltk.corpus import stopwords
 #nltk.download('punkt')
+#nltk.download('stopwords')
 import os
 import re, string
 import math
 
 def my_tokenize(text):
-#try getting rid of stop words, small words, stemming
- return word_tokenize(text)
+# return word_tokenize(text)
+ #tokens = [i for i in tokens if i not in stopwords.words('english')]
+ #tokens = [i for i in tokens if i not in string.punctuation]
+ tokens = word_tokenize(text)
+ s = dict((k,1) for k in stopwords.words('english'))
+ tokens = [i for i in tokens if i not in s]
+ s = dict((k,1) for k in string.punctuation)
+ tokens = [i for i in tokens if i not in s]
+# st = LancasterStemmer()
+# tokens = [st.stem(i) for i in tokens]
+ return tokens
 
-def get_train_category(corpus):
- f = "TC_provided/corpus"+str(corpus)+"_train.labels"
+def get_train_category(inputtrain):
  N = 0.0
  labels = {}
  classCounts = {}
- for line in open(f):
+ for line in open(inputtrain):
   N += 1.0
   (key,val) = line.split()
-  labels[key[16:]] = val
+  labels[key] = val
   if val in classCounts:
    classCounts[val] += 1.0
   else:
@@ -24,42 +35,38 @@ def get_train_category(corpus):
  return N, labels, classCounts
 
 def train(corpus):
- directory = "TC_provided/corpus"+str(corpus)+"/train/"
  N, labels, classCounts = get_train_category(corpus)
  catHash = {}
  for c in classCounts:
   catHash[c] = {}
 
- for filename in os.listdir(directory):
-  cat = labels[filename]
-  raw = open(directory+filename).read()
+ for filename in open(corpus):
+  cat = labels[filename[:-5]]
+  raw = open(filename[:-5]).read()
   text = my_tokenize(raw)
   for t in text:
     if t in catHash[cat]:
      catHash[cat][t] += 1.0
     else:
      catHash[cat][t] = 1.0
-
  return N, classCounts, catHash
 
-def classify(N, classCounts, catHash):
- directory = "TC_provided/corpus"+str(corpus)+"/test/"
- filelist = "TC_provided/corpus"+str(corpus)+"_test.list"
- outFile = open("corpus"+str(corpus)+"_predictions.labels", "w")
+def classify(N, classCounts, catHash, inputtest):
+ outFile = open(inputtest[:-5]+"_predictions.labels", "w")
  sumCatHash = {}
  for c in catHash:
   sumCatHash[c] = sum(catHash[c].values())
 
- for line in open(filelist):
-  raw = open("TC_provided/"+line.rstrip('\n')).read()
+ for line in open(inputtest):
+  raw = open(line.rstrip('\n')).read()
   text = my_tokenize(raw)
   f = {}
   testVocab = len(f)
   for token in text:
     if token in f:
-	f[token] += 1.0
+     f[token] += 1.0
     else:
-	f[token] = 1.0
+     f[token] = 1.0
 
   k = 0.15
   outProb = {} 
@@ -67,22 +74,23 @@ def classify(N, classCounts, catHash):
    prior = math.log(classCounts[c] / float(N))
    tokenProb = 0.0
    for token in f:
-	#token freq in training + k
-	num = catHash[c].get(token, 0.0) + k 
-	#total count in trainig + k*test vocab size 
-	den = sumCatHash[c] +  k*testVocab 
-	tokenProb += f[token]*math.log(num / den)
+     #token freq in training + k
+     num = catHash[c].get(token, 0.0) + k 
+     #total count in trainig + k*test vocab size 
+     den = sumCatHash[c] +  k*testVocab 
+     tokenProb += f[token]*math.log(num / den)
    outProb[c] = prior + tokenProb
 
   out = [k for k, v in outProb.iteritems() if v == max(outProb.values())]
+  #print line, outProb
   outFile.write(line.rstrip('\n'))
-  outFile.write(' ')
+  outFile.write(" ")
   outFile.write(''.join(out))
-  outFile.write('\n')
+  outFile.write("\n")
  outFile.close()
 
 
-corpus = 1
-N, classCounts, catHash = train(corpus)
-classify(N, classCounts, catHash)
-os.system("perl TC_provided/analyze.pl corpus"+str(corpus)+"_predictions.labels TC_provided/corpus"+str(corpus)+"_test.labels")
+inputtrain = raw_input("enter train list/labels file: ")
+inputtest = raw_input("enter test list file: ")
+N, classCounts, catHash = train(inputtrain)
+classify(N, classCounts, catHash, inputtest)
