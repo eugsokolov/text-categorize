1cdaf14350b543bc4337e934e182cc9748005a4f
eugsokolov <eugsokolov@gmail.com>
2016-03-23 00:20:56 -0400

    update

--- nb.py
+++ nb.py
@@ -1,5 +1,5 @@
 from nltk import word_tokenize
-from nltk.stem.lancaster import LancasterStemmer
+from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 #nltk.download('punkt')
 #nltk.download('stopwords')
@@ -7,16 +7,17 @@ import os
 import re, string
 import math
 
+#use NLTK word tokenizer, stopwords list and Porter Stemmer
 def my_tokenize(text):
 # return word_tokenize(text)
+ tokens = word_tokenize(text)
  #tokens = [i for i in tokens if i not in stopwords.words('english')]
  #tokens = [i for i in tokens if i not in string.punctuation]
- tokens = word_tokenize(text)
  s1 = dict((k,1) for k in stopwords.words('english'))
  s2 = dict((k,1) for k in string.punctuation)
  tokens = [i for i in tokens if i not in s1 and i not in s2]
-# st = LancasterStemmer()
-# tokens = [st.stem(i) for i in tokens]
+ st = PorterStemmer()
+ tokens = [st.stem(i) for i in tokens]
  return tokens
 
 def get_train_category(inputtrain):
@@ -67,8 +68,8 @@ def classify(N, classCounts, catHash, inputtest):
     else:
      f[token] = 1.0
 
-  k = 0.15
-  a = 1
+  k = 0.06
+  a = 1.0
   outProb = {} 
   for c in classCounts:
    prior = math.log(classCounts[c] / float(N))
@@ -81,8 +82,7 @@ def classify(N, classCounts, catHash, inputtest):
      tokenProb += f[token]*math.log(num / den)
    outProb[c] = prior + tokenProb
 
-  out = [k for k, v in outProb.iteritems() if v == max(outProb.values())]
-  #print line, outProb
+  out = [i for i, v in outProb.iteritems() if v == max(outProb.values())]
   outFile.write(line.rstrip('\n'))
   outFile.write(" ")
   outFile.write(''.join(out))
@@ -90,7 +90,11 @@ def classify(N, classCounts, catHash, inputtest):
  outFile.close()
 
 
-inputtrain = raw_input("enter train list/labels file: ")
-inputtest = raw_input("enter test list file: ")
+#inputtrain = raw_input("enter train list/labels file: ")
+#inputtest = raw_input("enter test list file: ")
+inputtrain = "corpus1_train.labels"
+inputtest = "corpus1_test.list"
 N, classCounts, catHash = train(inputtrain)
+ktest = [0.05, 0.055, 0.06, 0.065, 0.7]
 classify(N, classCounts, catHash, inputtest)
+os.system("perl analyze.pl corpus1_test_predictions.labels corpus1_test.labels")
